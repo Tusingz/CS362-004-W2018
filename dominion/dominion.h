3143e8535178bff326ec2288990f4cb115d146c6
tusingz <zttusing@protonmail.ch>
2018-01-21 21:08:09 -0800

    added changes to the header file

--- dominion/dominion.h
+++ dominion/dominion.h
@@ -128,4 +128,14 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+void adventurerAction(struct gameState *state);
+
+void smithyAction(struct gameState *state, int handPos);
+
+void villageAction(struct gameState *state, int handPos);
+
+void cutpurseAction(struct gameState *state, int handPos);
+
+void seaHagAction(struct gameState *state, int handPos);
+
 #endif
